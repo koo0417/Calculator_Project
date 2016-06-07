4c151d3da05b2df10e1c90329e481de3aaf306ad
MsvOrca <wjp229@naver.com>
2016-06-08 00:19:58 +0900

    ak

--- project_original.c
+++ project_original.c
@@ -1,1 +1,54 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+char num[10][50], dec[10][9];
+int i, d = -1;
+int order = 0;
+void comma(int i, int d, int order){//콤마 찍는 함수
+	int k, x;
+	for(k = 0; k <= 49; k++){//정수 부분 콤마 찍는 방법
+		printf("%c", num[order][k]);
+		if((i - k) % 3 == 1 && k < i-2)
+			printf(",");
+	}
+	if(d > 0){//소수 부분 콤마 찍는 방법
+		for(x = 0; x <= 8; x++){
+			printf("%c", dec[order][x]);
+			if(x % 3 == 2 && x < d-1)
+				printf(",");
+		}
+	}
+}
+void input(int order){//입력 함수
+	if(order % 2 == 0){
+		for(i = 0; i <= 50; i++){
+			scanf("%c", &num[order][i]);
+			if(num[order][i] == '.'){
+				for(d = 0; d <= 9; d++){
+					scanf("%c", &dec[order][d]);
+					if(dec[order][d] == ' ' || dec[order][d] == '\n')
+						break;
+				}
+				break;
+			}
+			if(num[order][i] == ' ' || num[order][i] == '\n')
+				break;
+		}
+	}
+	else
+		scanf("%c ", &num[order][0]);
+}
+int main()
+{
+	printf("(input) ");
+	for(order = 0; order < 10; order++){
+		input(order);
+		if(d < 0 && num[order][i] == '\n'){
+			break;
+		}
+		else if(dec[order][d] == '\n'){
+			break;
+		}
+	}
+	printf("= ");
+	return 0;
+}
